f209428dd0a9d122c117a11fa94dcd08bcf97983
Luke Lee <lukelee01@gmail.com>
2024-10-27 00:59:01 -0400

    update skaffold files

--- two-java-applications/scripts/minikube.sh
+++ two-java-applications/scripts/minikube.sh
@@ -3,5 +3,7 @@
 minikube delete
 minikube start
 minikube addons enable ingress
+#minikube addons enable registry
 skaffold config set --global local-cluster true
+#skaffold config set --global default-repo localhost:5000
 eval $(minikube -p custom docker-env)
